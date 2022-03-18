7b414843ed982124a37fc60b7520a432a2c9e331
Noelia Parada <noeliacastrelo@gmail.com>
2022-03-18 11:25:09 +0100

    verify route ok

--- src/services/auth.services.js
+++ src/services/auth.services.js
@@ -1,33 +1,30 @@
-import axios from 'axios'
+import axios from "axios";
 
 const service = axios.create({
-     baseURL:`${process.env.REACT_APP_BASE_URL}/auth`
-})
+  baseURL: `${process.env.REACT_APP_BASE_URL}/auth`,
+});
 
 //enviar el token en cada request que haga el usuario para checkear
 service.interceptors.request.use((config) => {
-    //buscar el token en el localStorage
-    const storedToken = localStorage.getItem('authToken')
-    //si el token existe lo añadimos a los headers del request 
-    config.headers = storedToken && { Authorization: `Bearer  + ${storedToken}`}
+  //buscar el token en el localStorage
+  const storedToken = localStorage.getItem("authToken");
+  //si el token existe lo añadimos a los headers del request //pasar la autorizacion como string  indicando el tipo de autenticacion. OJO CON LOS ESPACIOS!!
+  config.headers = storedToken && { Authorization: `Bearer ${storedToken}` };
+  console.log(storedToken);
 
-    return config
-})
+  return config;
+});
 
-const signupService = (user) =>{
-    return service.post("/signup", user)
-} 
+const signupService = (user) => {
+  return service.post("/signup", user);
+};
 
-const loginService = (user) =>{
-    return service.post("/login", user)
-}
+const loginService = (user) => {
+  return service.post("/login", user);
+};
 
-const verifyService = () =>{
-    return service.get("/verify")
-}
+const verifyService = () => {
+  return service.get("/verify");
+};
 
-export{
-    signupService,
-    loginService,
-    verifyService
-}
+export { signupService, loginService, verifyService };
