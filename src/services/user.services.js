750936675b8d4881546523ed618b7f0b1fc93b1c
Noelia Parada <noeliacastrelo@gmail.com>
2022-03-21 11:12:12 +0100

    user.services.js

---
+++ src/services/user.services.js
@@ -0,0 +1,19 @@
+import service from "./service";
+
+const URL = "/user";
+
+const getAllUsersService = () =>{
+    return service.get(`${URL}/`)
+}
+
+const getUsersDetailsService = (id) =>{
+    return service.get(`${URL}/${id}`)
+}
+
+
+
+
+export{ 
+    getAllUsersService,
+    getUsersDetailsService,
+}
