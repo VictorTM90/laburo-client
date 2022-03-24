3acc978e507c808416fc8cd1a366579c69b620b8
Noelia Parada <noeliacastrelo@gmail.com>
2022-03-24 12:36:13 +0100

    estilos en login y signup responsive

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import { verifyService } from "./services/auth.services";
 import Error from "./pages/Error";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
-import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import Task from "./pages/tasks/Task.jsx";
 
